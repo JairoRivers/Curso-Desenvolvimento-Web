a32a4f465159e8ff1115288464c763fee60bbe9d
Jairo Rios <jairo.rios@hotmail.com>
2024-08-27 14:45:37 -0300

    Aula

--- Java Script/4 - Orientação a Objetos/aula-35.js
+++ Java Script/4 - Orientação a Objetos/aula-35.js
@@ -2,3 +2,36 @@
 // organizar e abstrair coisas. Veja a definição formal: "O termo orientação a objetos
 // significa organizar o mundo real como uma coleção de objetos que incorporam estrutura
 // de dados e um conjunto de operações que manipulam estes dados.
+
+class Pessoa {
+    nome;
+    idade;
+    filhos;
+
+    constructor(nome, idade, filhos) {
+        this.nome = nome; // This refere-se ao objeto que é proprietário do método chamado
+        this.idade = idade;
+        this.filhos = filhos;
+    }
+
+    quantosFilhos() {
+        if (this.filhos) {
+            return this.filhos.length;
+        }
+        return 0;
+    }
+
+
+}
+
+const filho1 = new Pessoa('Gustavo', 13);
+const filho2 = new Pessoa('Gabriela', 10);
+console.log('Filhos1 tem: ', filho1.quantosFilhos());
+
+const pessoa = new Pessoa('Joao', 24, [filho1, filho2]);
+
+console.log('Quantos filhos?', pessoa.quantosFilhos());
+
+console.log(pessoa);
+
+
---
+++ Java Script/4 - Orientação a Objetos/aula-36.js
@@ -0,0 +1,39 @@
+// Herança
+
+class Usario {
+    email;
+    senha;
+
+    constructor(email, senha, nome) {
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+}
+
+class Administrador extends Usario {
+    permissoes;
+
+    constructor(email, senha, nome, permissoes) {
+        super(email, senha, nome);
+        this.permissoes = permissoes;
+    }
+
+}
+
+class Comprador extends Usario {
+    compras;
+}
+
+
+const admin = new Administrador('joao.gmail.com', '123456', 'Joao', [1, 6]);
+
+console.log(admin);
+
+
+const comprador = new Comprador();
+comprador.senha = '121212';
+
+console.log(comprador);
+
